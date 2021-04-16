a1242c521629ffc1f1034886b6d2bb97e325f50d
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-16 09:38:42 +0100

    Initial commit

---
+++ src/main/java/com/vinder/vinderbackend/VinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vinder.vinderbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinder/vinderbackend/VinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinder.vinderbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
