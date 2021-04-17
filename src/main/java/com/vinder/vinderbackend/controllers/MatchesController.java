0751c0c60360f3873fa741bb825ea4cd0161de45
Andrew Hunter <andrewhunter1612@gmail.com>
2021-04-17 15:47:30 +0100

    added repository and controller

---
+++ src/main/java/com/vinder/vinderbackend/controllers/MatchesController.java
@@ -0,0 +1,12 @@
+package com.vinder.vinderbackend.controllers;
+
+import com.vinder.vinderbackend.repositories.MatchesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MatchesController {
+
+    @Autowired
+    MatchesRepository matchesRepository;
+}
